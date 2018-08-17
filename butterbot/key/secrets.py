ae8aaba9ea8707b1087710566b58e6d26bfb540d
Hey-tom <tomirl2008@gmail.com>
2018-08-17 21:24:11 +0100

    Delete secrets.py

--- butterbot/key/secrets.py
+++
@@ -1,7 +0,0 @@
-APP_KEY_DEV = "gusridnrf2xOyYno"
-USERNAME = "Dog2017"
-PASSWORD = "07c18125**"
-APP_CERTS_DIR = '/Users/mac/PycharmProjects/testbot/certs/'
-APP_URL_LOGIN ='https://identitysso.betfair.com/api/certlogin'
-CERT ='/Users/mac/PycharmProjects/butterbot/butterbot/key/certs/client-2048.crt'
-KEY ='/Users/mac/PycharmProjects/butterbot/butterbot/key/certs/client-2048.key'
